80fb9824cfce4e16942030374a8a46511668ff10
Liam5490 <162510013+Liam5490@users.noreply.github.com>
2026-01-08 21:34:57 -0500

    Add files via upload
    This is a basic calculator program I made to simulate a simple calculation. I used this to teach myself the syntax of the C++ language

---
+++ calculator.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int function() {
+    string operation;
+    string repeat;
+    double number1, number2;
+
+
+    cout << "Enter first number: ";
+    cin >> number1;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> number2;
+
+    if (operation == "+") {
+        cout << number1 + number2;
+    } else if (operation == "-") {
+        cout << number1 - number2;
+    }
+    else if (operation == "*") {
+        cout << number1 * number2;
+    }
+    else if (operation == "/") {
+        cout << number1 / number2;
+    }
+    else {
+        cout << "not a recognized operation";
+    }
+
+    cout << "\nWould you like to use the calculator again(y/n)?:";
+    cin >> repeat;
+     if (repeat == "y") {
+        function();
+     }
+    else if (repeat == "n") {
+          cout << "Confirmed. Calculator program has ended...";
+    }
+    else {
+        cout << "Response is not recognized. Please try again.\n";
+        
+    }
+
+
+
+  
+  return 0;
+}
+
+
+
+
+
+int main()
+{
+ 
+string access;
+cout << "Would you like to use the calculator (y/n)?: ";
+cin >> access;
+
+if (access == "y")
+{
+function();
+}
+
+else if (access == "n")
+{
+    cout << "Confirmed. Calculator program has ended...";
+}
+
+else {
+    cout << "Response is not recognized. Please try again.\n";
+    main(); 
+}
+
+
+}
